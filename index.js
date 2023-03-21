041d00ce9ecf84e56a38c3d8f317eed4a78114cf
B CHUCHEL ASHISH <85539405+ashishrockz@users.noreply.github.com>
2023-03-21 23:16:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ const contentsRouter = require("./router/contents");
 app.use("/contents", contentsRouter);
 //app.use("/api/auth", authRoutes);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 1000;
 app.listen(port, console.log(`Listening on port ${port}...`));
