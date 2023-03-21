80c0e35cfb3ca6de2b921bf9dfc0b01a8912f786
B CHUCHEL ASHISH <85539405+ashishrockz@users.noreply.github.com>
2023-03-21 18:20:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connection = require("./db");
+const path =require('path');
 //const authRoutes = require("./routes/auth");
 
 // database connection
@@ -11,8 +12,12 @@ connection();
 // middlewares
 app.use(express.json());
 app.use(cors());
-
+app.use(express.static(path.join(__dirname,"./client/build")));
+app.get("*",function(req,res){
+    res.sendFile(path.join(__dirname,"./client/bulid/index.html"));
+});
 // routes
+
 
 const contentsRouter = require("./router/contents");
 app.use("/contents", contentsRouter);
